38b665dcac4ca6de51c3e66bcd00b06d2e97f727
jose <jmvr37@hotmail.com>
2020-11-24 13:14:51 -0800

    class review

--- compression.js
+++ compression.js
@@ -14,3 +14,21 @@ function compressString(text){
 }
 
 compressString(text);
+
+// anson's solution 
+function compressString2(text){
+    var output = '';
+    var count = 1;
+    for (var i = 0; i < text.length; i++){
+      if(text[i] === text[i + 1]){
+          count++;
+      } else {
+          output += count + text[i]
+          count = 1;
+      }
+    }
+    return output;
+ }
+
+ console.log(compressString2(text))
+ console.log("QQQUUUUUUUlllllllqqQQQQQQQQQTTA")
